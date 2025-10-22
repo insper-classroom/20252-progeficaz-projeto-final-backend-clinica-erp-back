4af412f86013221610232aafaa410b6b17eeddd4
erickob <erickob@al.insper.edu.br>
2025-10-22 17:19:49 -0300

    feat: primeiras rotas

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from bson import ObjectId  
 
 load_dotenv('.cred')
 
@@ -19,22 +20,50 @@ def connect_db():
 
 app = Flask(__name__)
 
-@app.route('/alunos', methods=['GET'])
-def get_alunos():
+@app.route('/medicos', methods=['GET'])
+def get_medicos():
     db = connect_db()
     if db is None:
         return {"erro": "Erro ao conectar ao banco de dados"}, 500
 
     try:
-        collection = db['alunos']
-        alunos_cursor = collection.find({}, {"_id": 0})  # Remove o campo _id da resposta
-        alunos = list(alunos_cursor)
+        collection = db['medicos']
+        medicos_cursor = collection.find({})  
+        medicos = []
+        for medico in medicos_cursor:
+            medico['_id'] = str(medico['_id'])  
+            medicos.append(medico)
 
-        if not alunos:
-            return {"erro": "Nenhum aluno encontrado"}, 404
-        return {"alunos": alunos}, 200
+        if not medicos:
+            return {"erro": "Nenhum médico encontrado"}, 404
+        return {"medicos": medicos}, 200
     except Exception as e:
-        return {"erro": f"Erro ao consultar alunos: {str(e)}"}, 500
+        return {"erro": f"Erro ao consultar médicos: {str(e)}"}, 500
+    
+@app.route('/medicos/<string:id>', methods=['GET'])
+def get_medico_id():
+    db = connect_db()
+    if db is None:
+        return {"erro": "Erro ao conectar ao banco de dados"}, 500
+
+    try:
+        if not ObjectId.is_valid(id):
+            return {"erro": "ID inválido"}, 400
+
+        collection = db['medicos']
+        medico = collection.find_one({"_id": ObjectId(id)})
+
+        if not medico:
+            return {"erro": "Médico não encontrado"}, 404
+
+        medico['_id'] = str(medico['_id'])  
+        return {"medico": medico}, 200
+    except Exception as e:
+        return {"erro": f"Erro ao consultar médico: {str(e)}"}, 500
+
+    
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
